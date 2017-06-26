d205000c34c559587b4d54f325fffc8965a1e9ec
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-26 16:05:21 -0400

    Change heading on button click attempt 1

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+export TERM=cygwin
+export EDITOR=code
+
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('It worked!')
+function changeHeading(){
+    const heading = document.querySelector("h1")
+    heading.textContent = 'Ye Old Person Stats'
+}
+const btn = document.querySelector('button')
+//btn.addEventListener('click', )
