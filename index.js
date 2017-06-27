a7dce62b8ccc0225dc549037f3309fefa588011a
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-27 14:37:46 -0400

    Added age input and fixed a bug

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ function changeHeading(ev){
     ev.preventDefault()
     const para = document.querySelector("#stats")
     const next = ev.target.personName.value
+    const age = ev.target.personAge.value
     const color = ev.target.personInfo.value
     
     if(i!=0){
         
         const curr = para.innerHTML
-        para.innerHTML = "<p>"+next+'</p>'+curr
+        para.innerHTML = "<p>"+next+', '+age+'</p>'+curr
         const pa = para.querySelector('p')
         pa.style.color = color
     }else{
-        para.innerHTML = '<p>'+next+'</p>'
+        para.innerHTML = '<p>'+next+', '+age+'</p>'
+        const pa = para.querySelector('p')
+        pa.style.color = color
         i++
     }
+   
 }   
 let i = 0;
 const personForm = document.querySelector("#person-form")
