9e9b2df5ac952939d755ec9c1f1f9edc12b1bed3
Keith Hanson <hanson2@rose-hulman.edu>
2017-06-27 14:29:06 -0400

    names are in independent paragraphs with independent colors

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 function changeHeading(ev){
     ev.preventDefault()
-    const para = document.querySelector("#para")
+    const para = document.querySelector("#stats")
     const next = ev.target.personName.value
     const color = ev.target.personInfo.value
-    para.style.color = color
+    
     if(i!=0){
         
-        const curr = para.textContent
-        para.textContent = curr+", "+next
+        const curr = para.innerHTML
+        para.innerHTML = "<p>"+next+'</p>'+curr
+        const pa = para.querySelector('p')
+        pa.style.color = color
     }else{
-        para.textContent = next
+        para.innerHTML = '<p>'+next+'</p>'
         i++
     }
-}
+}   
 let i = 0;
 const personForm = document.querySelector("#person-form")
 personForm.addEventListener('submit', changeHeading)
